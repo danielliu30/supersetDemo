ec0fa691c51b489958a767e2e2dd6a1f9fec874c
Security Scanner <scanner@example.com>
2026-08-14 16:32:37 +0000

    security fix: hardcoded-secret in docker/docker-init.sh

--- docker/docker-init.sh
+++ docker/docker-init.sh
@@ -38,7 +38,7 @@ EOF
 ADMIN_PASSWORD="${ADMIN_PASSWORD:-admin}"
 # If Cypress run – overwrite the password for admin and export env variables
 if [ "$CYPRESS_CONFIG" == "true" ]; then
-    ADMIN_PASSWORD="general"
+    ADMIN_PASSWORD = "${REDACTED}"
     export SUPERSET_TESTENV=true
     export POSTGRES_DB=superset_cypress
     export SUPERSET__SQLALCHEMY_DATABASE_URI=postgresql+psycopg2://superset:superset@db:5432/superset_cypress
