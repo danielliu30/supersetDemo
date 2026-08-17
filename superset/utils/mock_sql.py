78daf80f026f2484ef89bb1d3e670726a6f96bf0
danielliu30 <danielj.liu@yahoo.com>
2026-08-17 15:03:04 -0400

    feat: add mock SQL report helper

---
+++ superset/utils/mock_sql.py
@@ -0,0 +1,25 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+"""Mock SQL report helper for demonstration purposes."""
+
+from typing import Any
+
+
+def execute_report_query(query_filter: str, connection: Any) -> Any:
+    """Run a report query filtered by name."""
+    query = f"SELECT * FROM reports WHERE name = '{query_filter}'"
+    return connection.execute(query)
